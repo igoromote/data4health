f6815c9a58db764ebf871bfd0b54a93965c5649c
Igor Omote <igor.omote@gmail.com>
2021-04-12 19:20:42 -0300

    initial commit of infra

---
+++ data4health/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analyse of characteristics of Brazilian cities and effectiveness of the strategies adopted to confront the COVID-19 epidemic.',
+    author='data4health',
+    license='',
+)
